171a80cad502424dea4c39d20e93581387a56d43
alexthedar <alex.castaneda@gmail.com>
2016-06-01 21:39:27 -0700

    checkpoint 28 done

---
+++ Blocstagram/Media.m
@@ -0,0 +1,14 @@
+//
+//  Media.m
+//  Blocstagram
+//
+//  Created by Alexandar on 6/1/16.
+//  Copyright © 2016 Alexandar. All rights reserved.
+//
+
+#import "Media.h"
+ #import "User.h"
+
+@implementation Media
+
+@end
---
+++ Blocstagram/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Blocstagram
+//
+//  Created by Alexandar on 6/1/16.
+//  Copyright © 2016 Alexandar. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
