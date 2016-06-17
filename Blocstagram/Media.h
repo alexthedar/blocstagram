310d7b8386373484eaf4f59c7e0a4aaf271134bb
alexthedar <alex.castaneda@gmail.com>
2016-06-17 05:55:21 -0700

    checkpoint 38 done

--- Blocstagram/Media.h
+++ Blocstagram/Media.h
@@ -7,6 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+typedef NS_ENUM(NSInteger, MediaDownloadState) {
+    MediaDownloadStateNeedsImage             = 0,
+    MediaDownloadStateDownloadInProgress     = 1,
+    MediaDownloadStateNonRecoverableError    = 2,
+    MediaDownloadStateHasImage               = 3
+};
 
 @class User;
 
@@ -18,5 +24,6 @@
 @property (nonatomic, strong) UIImage *image;
 @property (nonatomic, strong) NSString *caption;
 @property (nonatomic, strong) NSArray *comments;
+@property (nonatomic, assign) MediaDownloadState downloadState;
 - (instancetype) initWithDictionary:(NSDictionary *)mediaDictionary;
 @end
