c4956f8b8dce426a343da781c2a7ddf7ddc4daaa
alexthedar <alex.castaneda@gmail.com>
2016-06-24 06:09:04 -0700

    checkpoint-43 almost done

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Alexandar on 6/24/16.
+//  Copyright © 2016 Alexandar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+@end
